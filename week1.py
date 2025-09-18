693630fe24c5e55b94c6c2444c0948f0a0f952a0
유형욱 <hy_uk000@naver.com>
2025-09-18 22:42:14 +0900

    week1

--- week1.py
+++ week1.py
@@ -1,3 +1,58 @@
-a=100
-b=12
-print(a+b)
+# # 2557
+# print("Hello World!")
+
+# # 1000, 1001, 10998 MD
+# a,b=map(int,input().split())
+# print(a+b,a-b,a*b)
+
+# # 10869
+# a,b=map(int,input().split())
+# print(a+b,a-b,a*b,int(a/b),a%b)
+
+# # 10926 MD
+# a = input()
+# print(a +'??!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
