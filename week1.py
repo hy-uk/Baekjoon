8740f7d576a6676cf951917fb24e165405746770
유형욱 <hy_uk000@naver.com>
2025-09-19 01:03:41 +0900

    1일차 끝

--- week1.py
+++ week1.py
@@ -5,19 +5,20 @@
 # a,b=map(int,input().split())
 # print(a+b,a-b,a*b)
 
-# # 10869
+# # 10869 MD
 # a,b=map(int,input().split())
-# print(a+b,a-b,a*b,int(a/b),a%b)
+# print(a+b,a-b,a*b,a//b,a%b)
 
 # # 10926 MD
 # a = input()
 # print(a +'??!')
 
-
-
-
-
-
+# # 10430 MD
+# A, B, C = map(int, input().split())
+# print((A+B)%C)
+# print(((A%C)+(B%C))%C)
+# print((A*B)%C)
+# print(((A%C)*(B%C))%C)
 
 
 
