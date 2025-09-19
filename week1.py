4a6eedde03580a856bff16da9b4515d2b6722fcc
유형욱 <hy_uk000@naver.com>
2025-09-19 14:30:50 +0900

    약간 수정

--- week1.py
+++ week1.py
@@ -1,19 +1,19 @@
 # # 2557
 # print("Hello World!")
 
-# # 1000, 1001, 10998 MD
+# # 1000, 1001, 10998 *MD
 # a,b=map(int,input().split())
 # print(a+b,a-b,a*b)
 
-# # 10869 MD
+# # 10869 *MD
 # a,b=map(int,input().split())
 # print(a+b,a-b,a*b,a//b,a%b)
 
-# # 10926 MD
+# # 10926 *MD
 # a = input()
 # print(a +'??!')
 
-# # 10430 MD
+# # 10430 *MD
 # A, B, C = map(int, input().split())
 # print((A+B)%C)
 # print(((A%C)+(B%C))%C)
